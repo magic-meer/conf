77c0a1c94e443181feb4fa7b992496c94de5b119
Meher Ali <meherali.meer@gmail.com>
2026-07-31 10:49:47 +0500

    updated foot/default.nix

--- home/meher/foot/default.nix
+++ home/meher/foot/default.nix
@@ -1,64 +1,43 @@
 {
-  pkgs,
-  ...
-}:
+   pkgs,
+   ...
+}: {
+   programs.foot = {
+      enable = true;
 
-let
-  inherit (pkgs) lib;
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+      enableZshInetegration = true;
 
-  generateIni = sections:
-    lib.concatStringsSep "\n\n" (lib.mapAttrsToList (sectionName: values:
-      let
-        kvps = lib.mapAttrsToList (key: value:
-          if lib.isList value then
-            "${key}=${lib.concatStringsSep " " (map toString value)}"
-          else if value == true then
-            "${key}=yes"
-          else if value == false then
-            "${key}=no"
-          else
-            "${key}=${toString value}"
-        ) values;
-      in
-      "[${sectionName}]\n" + lib.concatStringsSep "\n" kvps
-    ) (lib.filterAttrs (_: v: v != {}) sections));
-
-  keybinds = import ./keybinds.nix;
-
-  allSections =
-    if keybinds == {}
-    then footConfig
-    else footConfig // { "key-bindings" = keybinds; };
-
-  footConfig = {
-    main = {
-      font = "JetBrainsMono Nerd Font:size=11";
-      line-height = "14";
-      letter-spacing = "1px";
-      pad = "8x8 center";
-      initial-window-size-chars = "100x30";
-      initial-window-mode = "windowed";
-      dpi-aware = "yes";
-    };
-    cursor = {
-      style = "underline";
-      blink = true;
-      blink-rate = 500;
-    };
-    mouse = {
-      hide-when-typing = true;
-      alternate-scroll-mode = true;
-    };
-    csd = {
-      preferred = "none";
-      size = 0;
-      border-width = 0;
-    };
-    "colors-dark" = {
-      alpha = "0.35";
-      blur = true;
-    };
-  };
-in {
-  xdg.configFile."foot/foot.ini".text = generateIni allSections;
+      settings = {
+         main = {
+               font = "JetBrainsMono Nerd Font:size=11";
+               line-height = "14";
+               letter-spacing = "1px";
+               pad = "8x8 center";
+               initial-window-size-chars = "100x30";
+               initial-window-mode = "windowed";
+               dpi-aware = "yes";
+            };
+            cursor = {
+               style = "underline";
+               blink = true;
+               blink-rate = 500;
+            };
+            mouse = {
+               hide-when-typing = true;
+               alternate-scroll-mode = true;
+            };
+            csd = {
+               preferred = "none";
+               size = 0;
+               border-width = 0;
+            };
+            "colors-dark" = {
+               alpha = "0.35";
+               blur = true;
+            };
+         };
+      };
+   };
 }
