fd6434a94a222924ae5483658cbbec7705e55e6a
Meher Ali <meherali.meer@gmail.com>
2026-08-07 19:13:00 +0500

    added gimp to packages

--- home/meher/packages.nix
+++ home/meher/packages.nix
@@ -22,5 +22,6 @@ home.packages = with pkgs; [
          jdk
          steam-run
          quickshell
+         gimp
        ];
 }
--- home/meher/tlauncher/default.nix
+++ home/meher/tlauncher/default.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 
 {
   home.file.".local/share/tlauncher/TLauncher.jar".source =
@@ -15,6 +20,27 @@
         -jar "$HOME/.local/share/tlauncher/TLauncher.jar" "$@"
     '';
   };
+
+  # Read-only game assets, symlinked into the minecraft game dir so they are
+  # managed declaratively here and removed/updated on rebuild.
+  home.file.".minecraft/mods".source = ./mods;
+  home.file.".minecraft/resourcepacks".source = ./resourcepacks;
+  home.file.".minecraft/shaderpacks".source = ./shaderpacks;
+
+  # Worlds must stay writable, so `saves` links straight back to this repo
+  # folder instead of the (read-only) nix store.
+  home.activation.linkTlauncherSaves = lib.hm.dag.entryAfter [
+    "linkGeneration"
+  ] ''
+    repoDir="${config.home.homeDirectory}/nixconfig/home/meher/tlauncher/saves"
+    target="${config.home.homeDirectory}/.minecraft/saves"
+
+    $DRY_RUN_CMD mkdir -p "$repoDir"
+    if [[ ! -L "$target" && -e "$target" ]]; then
+      $DRY_RUN_CMD mv "$target" "$target.home-backup.$(date +%s)"
+    fi
+    $DRY_RUN_CMD ln -sfn "$repoDir" "$target"
+  '';
 
   xdg.dataFile."applications/tlauncher.desktop".text = ''
     [Desktop Entry]
