51ef59edf7696809d9de452dacbb0b1645c5ce9e
Meher Ali <meherali.meer@gmail.com>
2026-08-07 10:02:13 +0500

    add swaylock lock screen config

---
+++ home/meher/swaylock.nix
@@ -0,0 +1,40 @@
+{
+  ...
+}: {
+  home.file.".config/swaylock/config".text = ''
+    color=#1b1d26
+
+    image=/home/meher/wallpapers/static/wp12974034-sky-aesthetic-4k-wallpapers.jpg
+    scaling=fill
+
+    font=JetBrainsMono Nerd Font
+    font-size=32
+
+    indicator-idle-visible
+    indicator-caps-lock
+    indicator-radius=100
+    indicator-thickness=6
+
+    line-uses-inside
+
+    ring-color=#8a8a8a
+    inside-color=#1b1d2600
+    key-hl-color=#ffffff
+    bs-hl-color=#8a8a8a
+    separator-color=#00000000
+
+    ring-ver-color=#8a8a8a
+    inside-ver-color=#1b1d26
+
+    ring-wrong-color=#fb4934
+    inside-wrong-color=#fb4934
+
+    text-color=#e6e6e6
+    text-ver-color=#ffffff
+    text-wrong-color=#ffffff
+    text-clear-color=#ffffff
+
+    show-failed-attempts
+    ignore-empty-password
+  '';
+}
