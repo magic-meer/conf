f20135439275da42938a2482a584242fdf084a1a
Meher Ali <meherali.meer@gmail.com>
2026-08-23 19:15:20 +0500

    updated rebuild command

--- home/meher/shell.nix
+++ home/meher/shell.nix
@@ -7,7 +7,7 @@
     lh = "ls -h";
     lha = "ls -alh";
     cdd = "cd ..";
-    update = "sudo nixos-rebuild switch --flake ~/nixconfig#jinnnn";
+    rebuild-system = "sudo nixos-rebuild switch --flake ~/nixconfig#jinnnn";
     gita = "git add .";
     gitc = "git commit -m";
     gits = "git status";
