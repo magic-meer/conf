9922582a8a5864d9fc5075c8dc6dadaf6f456cd0
Meher Ali <meherali.meer@gmail.com>
2026-08-28 21:34:21 +0500

    niri: move workspace switching to Mod+J/K, window focus to Mod+Y/N

--- home/meher/niri/keybinds.nix
+++ home/meher/niri/keybinds.nix
@@ -32,14 +32,14 @@
       repeat = false;
     };
 
-    # Window focus (vim-style h/j/k/l)
+    # Window focus (vim-style arrow cluster: H/L horizontal, Y/N vertical)
     "Mod+Left".action.focus-column-left = [];
     "Mod+Down".action.focus-window-down = [];
     "Mod+Up".action.focus-window-up = [];
     "Mod+Right".action.focus-column-right = [];
     "Mod+H".action.focus-column-left = [];
-    "Mod+J".action.focus-window-down = [];
-    "Mod+K".action.focus-window-up = [];
+    "Mod+Y".action.focus-window-up = [];
+    "Mod+N".action.focus-window-down = [];
     "Mod+L".action.focus-column-right = [];
 
     # Move windows (mirrors the focus keys)
@@ -48,8 +48,8 @@
     "Mod+Ctrl+Up".action.move-window-up = [];
     "Mod+Ctrl+Right".action.move-column-right = [];
     "Mod+Ctrl+H".action.move-column-left = [];
-    "Mod+Ctrl+J".action.move-window-down = [];
-    "Mod+Ctrl+K".action.move-window-up = [];
+    "Mod+Ctrl+N".action.move-window-down = [];
+    "Mod+Ctrl+Y".action.move-window-up = [];
     "Mod+Ctrl+L".action.move-column-right = [];
 
     # Column first/last
@@ -58,40 +58,19 @@
     "Mod+Ctrl+Home".action.move-column-to-first = [];
     "Mod+Ctrl+End".action.move-column-to-last = [];
 
-    # Monitor focus
-    "Mod+Shift+Left".action.focus-monitor-left = [];
-    "Mod+Shift+Down".action.focus-monitor-down = [];
-    "Mod+Shift+Up".action.focus-monitor-up = [];
-    "Mod+Shift+Right".action.focus-monitor-right = [];
-    "Mod+Shift+H".action.focus-monitor-left = [];
-    "Mod+Shift+J".action.focus-monitor-down = [];
-    "Mod+Shift+K".action.focus-monitor-up = [];
-
-    # Move column to monitor
-    "Mod+Shift+Ctrl+Left".action.move-column-to-monitor-left = [];
-    "Mod+Shift+Ctrl+Down".action.move-column-to-monitor-down = [];
-    "Mod+Shift+Ctrl+Up".action.move-column-to-monitor-up = [];
-    "Mod+Shift+Ctrl+Right".action.move-column-to-monitor-right = [];
-    "Mod+Shift+Ctrl+H".action.move-column-to-monitor-left = [];
-    "Mod+Shift+Ctrl+J".action.move-column-to-monitor-down = [];
-    "Mod+Shift+Ctrl+K".action.move-column-to-monitor-up = [];
-    "Mod+Shift+Ctrl+L".action.move-column-to-monitor-right = [];
-
     # Workspace navigation (previous/next)
     "Mod+Page_Down".action.focus-workspace-down = [];
     "Mod+Page_Up".action.focus-workspace-up = [];
+    "Mod+J".action.focus-workspace-down = [];
+    "Mod+K".action.focus-workspace-up = [];
     "Mod+U".action.focus-workspace-down = [];
     "Mod+I".action.focus-workspace-up = [];
     "Mod+Ctrl+Page_Down".action.move-column-to-workspace-down = [];
     "Mod+Ctrl+Page_Up".action.move-column-to-workspace-up = [];
+    "Mod+Ctrl+J".action.move-column-to-workspace-down = [];
+    "Mod+Ctrl+K".action.move-column-to-workspace-up = [];
     "Mod+Ctrl+U".action.move-column-to-workspace-down = [];
     "Mod+Ctrl+I".action.move-column-to-workspace-up = [];
-
-    # Move workspace between monitors
-    "Mod+Shift+Page_Down".action.move-workspace-down = [];
-    "Mod+Shift+Page_Up".action.move-workspace-up = [];
-    "Mod+Shift+U".action.move-workspace-down = [];
-    "Mod+Shift+I".action.move-workspace-up = [];
 
     # Scroll wheel workspace switching
     "Mod+WheelScrollDown" = {
